6bf209d1d363eaeaffd7e4a74bd6e18c644e306e
Bari <brownjab21@gmail.com>
2023-02-24 08:12:15 -0600

    fix: undefined value function

--- filter/filter_test.go
+++ filter/filter_test.go
@@ -20,14 +20,70 @@ func traverse(n *Node) {
 	}
 }
 
-func TestFilter(t *testing.T) {
+func TestAndFilter(t *testing.T) {
 	f := New(
-		// And(
-		// 	To("0xdc6C276D357e82C7D38D73061CEeD2e33990E5bC"),
-		// 	From("0x34Be5b8C30eE4fDe069DC87D989686aBE98abcde"),
-		// ),
-		Value(big.NewInt(10000)),
+		And(
+			To("0xdc6C276D357e82C7D38D73061CEeD2e33990E5bC"),
+			From("0x34Be5b8C30eE4fDe069DC87D989686aBE98abcde"),
+		),
 	)
+
+	traverse(f.Root)
+
+	v, err := json.MarshalIndent(f, "", "    ")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	fmt.Println(string(v))
+}
+
+func TestOrFilter(t *testing.T) {
+	f := New(
+		Or(
+			To("0xdc6C276D357e82C7D38D73061CEeD2e33990E5bC"),
+			To("0x34Be5b8C30eE4fDe069DC87D989686aBE98abcde"),
+		),
+	)
+
+	traverse(f.Root)
+
+	v, err := json.MarshalIndent(f, "", "    ")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	fmt.Println(string(v))
+}
+
+func TestValueEqFilter(t *testing.T) {
+	f := New(ValueEq(big.NewInt(10000)))
+
+	traverse(f.Root)
+
+	v, err := json.MarshalIndent(f, "", "    ")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	fmt.Println(string(v))
+}
+
+func TestValueLteFilter(t *testing.T) {
+	f := New(ValueLte(big.NewInt(10000)))
+
+	traverse(f.Root)
+
+	v, err := json.MarshalIndent(f, "", "    ")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	fmt.Println(string(v))
+}
+
+func TestValueGteFilter(t *testing.T) {
+	f := New(ValueGte(big.NewInt(10000)))
 
 	traverse(f.Root)
 
